5c3451983df52e129446d6f5c285d87098a084f4
Jonathan Morris-Pocock <jon.morris.pocock@mail.utoronto.ca>
2016-03-14 17:21:09 -0400

    Added basic flashcard creation

---
+++ flashcards/data/create-flashcard.js
@@ -0,0 +1,24 @@
+var front = document.getElementById("front");
+var back = document.getElementById("back");
+var submit = document.getElementById("submit");
+var close = document.getElementById("close");
+
+// Send the front and back flashcard text to index.js
+submit.addEventListener("click", function() {
+  self.port.emit("create-flashcard-submit", front.value, back.value);
+}, false);
+
+// Hide the popup when then "close" link is clicked
+close.addEventListener("click", function() {
+  self.port.emit("create-flashcard-close");
+}, false);
+
+// Set the "Front" field to the highlighted text, clear
+// the "Back" field, and set focus to it when the
+// popup opens
+self.port.on("set-front", function setFront(text) {
+  front.value = text;
+  back.value = "";
+  back.focus();
+});
+
--- flashcards/index.js
+++ flashcards/index.js
@@ -1,5 +1,35 @@
 var self = require("sdk/self");
 
+// Initialize the flashcard persistence object
+var ss = require("sdk/simple-storage");
+if (typeof ss.storage.flashcards == "undefined") {
+  ss.storage.flashcards = [];
+}
+
+// Create the "Create Flashcard" popup window
+var create_flashcard = require("sdk/panel").Panel({
+  width: 500,
+  height: 390,
+  contentURL: self.data.url("create-flashcard.html"),
+  contentScriptFile: self.data.url("create-flashcard.js"),
+  contentStyle: "body { margin: 10px; }"
+});
+
+// Store the new flashcard and hide the creation window
+// when the "Create Flashcard" form is submitted
+create_flashcard.port.on("create-flashcard-submit", function(front, back) {
+  ss.storage.flashcards.push({front: front, back: back});
+  create_flashcard.hide();
+});
+
+// Hide the creation window when the "Create Flashcard" form
+// is submitted
+create_flashcard.port.on("create-flashcard-close", function() {
+  create_flashcard.hide();
+});
+
+// Create the right-click menu entry for flashcard creation,
+// and set the "Front" field to the highlighted text
 var contextMenu = require("sdk/context-menu");
 var menuItem = contextMenu.Item({
   label: "Create Flashcard",
@@ -9,7 +39,8 @@ var menuItem = contextMenu.Item({
                  '  self.postMessage(text);' +
                  '});',
   onMessage: function (text) {
-    console.log(text);
+    create_flashcard.show();
+    create_flashcard.port.emit("set-front", text);
   }
 });
 
