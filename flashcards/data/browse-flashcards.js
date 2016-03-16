5e64c69165f6bdbce3f820a5814599afcc7f6bca
Jonathan Morris-Pocock <jon.morris.pocock@mail.utoronto.ca>
2016-03-16 15:42:17 -0400

    Added browsing/updating functionality.

---
+++ flashcards/data/browse-flashcards.js
@@ -0,0 +1,82 @@
+var front = document.getElementById('front');
+var back = document.getElementById('back');
+var update = document.getElementById('update');
+var close = document.getElementById('close');
+var previous = document.getElementById('previous');
+var next = document.getElementById('next');
+var page = document.getElementById('page');
+var details = document.getElementById('details');
+var noFlashcards = document.getElementById('no-flashcards');
+
+var flashcards;
+var currentFlashcard;
+
+/* Store the flashcards array from index.js. Display
+ * an error message if there are not yet any
+ * flashcards to display. Otherwise, display the first
+ * flashcard's details.
+ */
+self.port.on('flashcards', function setFlashcards(flashcard_array) {
+  flashcards = flashcard_array;
+  if (flashcards.length == 0) {
+  	details.style.display = 'none';
+  	noFlashcards.style.display = 'block';
+  } else {
+  	currentFlashcard = 0;
+  	enableDisablePreviousNext();
+  	front.value = flashcards[currentFlashcard].front;
+  	back.value = flashcards[currentFlashcard].back;
+  	details.style.display = 'block';
+  	noFlashcards.style.display = 'none';
+  }
+});
+
+/* Hide the popup when then 'close' link is clicked. */
+close.addEventListener('click', function() {
+  self.port.emit('browse-flashcards-close');
+}, false);
+
+/* Update the current flashcard when the 'update' button
+ * is clicked.
+ */
+update.addEventListener('click', function() {
+  flashcards[currentFlashcard].front = front.value;
+  flashcards[currentFlashcard].back = back.value;
+  self.port.emit('update-flashcard', currentFlashcard, flashcards[currentFlashcard]);
+}, false);
+
+/* Disable the 'previous' link if on the first flashcard
+ * and disable the 'next' link if on the last flashcard.
+ */
+function enableDisablePreviousNext() {
+  if (currentFlashcard == 0) {
+  	previous.className = 'disabled';
+  } else {
+  	previous.className = '';
+  }
+
+  if (currentFlashcard == flashcards.length - 1) {
+  	next.className = 'disabled';
+  } else {
+  	next.className = '';
+  }
+  front.focus();
+  page.innerHTML = (currentFlashcard + 1) + '/' + flashcards.length;
+}
+
+/* Display the next flashcard's details. */
+next.addEventListener('click', function() {
+  currentFlashcard++;
+  enableDisablePreviousNext();
+  front.value = flashcards[currentFlashcard].front;
+  back.value = flashcards[currentFlashcard].back;
+}, false);
+
+/* Display the previous flashcard's details. */
+previous.addEventListener('click', function() {
+  currentFlashcard--;
+  enableDisablePreviousNext();
+  front.value = flashcards[currentFlashcard].front;
+  back.value = flashcards[currentFlashcard].back;
+}, false);
+
--- flashcards/index.js
+++ flashcards/index.js
@@ -1,12 +1,12 @@
 var self = require("sdk/self");
 
-// Initialize the flashcard persistence object
+/* Initialize the flashcard peristence object. */
 var ss = require("sdk/simple-storage");
 if (typeof ss.storage.flashcards == "undefined") {
   ss.storage.flashcards = [];
 }
 
-// Create the "Create Flashcard" popup window
+/* Create the "Create Flashcard" popup panel. */
 var create_flashcard = require("sdk/panel").Panel({
   width: 500,
   height: 390,
@@ -15,21 +15,24 @@ var create_flashcard = require("sdk/panel").Panel({
   contentStyle: "body { margin: 10px; }"
 });
 
-// Store the new flashcard and hide the creation window
-// when the "Create Flashcard" form is submitted
+/* Store the new flashcard and hide the creation window
+ * when the "Create Flashcard" form is submitted.
+ */
 create_flashcard.port.on("create-flashcard-submit", function(front, back) {
   ss.storage.flashcards.push({front: front, back: back});
   create_flashcard.hide();
 });
 
-// Hide the creation window when the "Create Flashcard" form
-// is submitted
+/* Hide the creation window when the "Create Flashcard" form
+ * is closed.
+ */
 create_flashcard.port.on("create-flashcard-close", function() {
   create_flashcard.hide();
 });
 
-// Create the right-click menu entry for flashcard creation,
-// and set the "Front" field to the highlighted text
+/* Create the right-click menu entry for flashcard creation,
+ * and set the "Front" field to the highlighted text.
+ */
 var contextMenu = require("sdk/context-menu");
 var menuItem = contextMenu.Item({
   label: "Create Flashcard",
@@ -39,8 +42,42 @@ var menuItem = contextMenu.Item({
                  '  self.postMessage(text);' +
                  '});',
   onMessage: function (text) {
-    create_flashcard.show();
     create_flashcard.port.emit("set-front", text);
+    create_flashcard.show();
   }
 });
 
+/* Create the "Browse Flashcards" popup panel. */
+var browse_flashcards = require("sdk/panel").Panel({
+  width: 500,
+  height: 390,
+  contentURL: self.data.url("browse-flashcards.html"),
+  contentScriptFile: self.data.url("browse-flashcards.js"),
+  contentStyle: "body { margin: 10px; }"
+});
+
+/* Hide the browsing panel when the "Browse Flashcards" form
+ * is closed.
+ */
+browse_flashcards.port.on("browse-flashcards-close", function() {
+  browse_flashcards.hide();
+});
+
+browse_flashcards.port.on("update-flashcard", function(index, value) {
+  ss.storage.flashcards[index] = value;
+});
+
+/* Create the addon button for browsing flashcards. */
+var button = require('sdk/ui/button/action').ActionButton({
+  id: 'browse-flashcards',
+  label: 'Browse Flashcards',
+  icon: {
+    '16': './icon-16.png',
+    '32': './icon-32.png',
+    '64': './icon-64.png'
+  },
+  onClick: function(state) {
+    browse_flashcards.port.emit("flashcards", ss.storage.flashcards);
+    browse_flashcards.show();
+  }
+});
